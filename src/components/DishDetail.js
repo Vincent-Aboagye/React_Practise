017409e498d30c0056f3d5ac4b6be19659516829
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-11-12 14:29:08 +0000

    Single Page Application not complete

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
  
 function RenderDish({dish}) {
         if (dish != null){
             return (
+                <div className="col-12 col-md-5 m-1">
                     <Card>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardBody>
@@ -14,6 +16,8 @@ function RenderDish({dish}) {
                         </CardBody>
 
                     </Card>
+                    {console.log(`${dish}`)}
+                    </div>
             )
             }
             else{
@@ -27,48 +31,57 @@ function RenderDish({dish}) {
             
     }
     
-function DishComments({comments}){
-        const comt = comments.comments.map((comment)=>{
-
-            if (comment !=null){
-
-            return(
-                <ul key={comment.id} className="list-unstyled">
-                <li>
-                    {comment.comment}                  
-                </li>
-                <li>
-                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric',month: 'short',day: '2-digit'}).format(new Date(Date.parse(comment.date)) )}
-                </li>
-                </ul>
+function RenderComments({comments}){
+            if ( comments !=null ){
+                return(  
+                    <div className="col-12 col-md-5 m-1">    
+                        <h4>Comments</h4>
+                        <ul className="list-unstyled">
+                            {comments.map((comment)=>{
+                                return(
+                                    <li  key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p> -- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric',month: 'short',day: '2-digit'}).format(new Date(Date.parse(comment.date)) )}
+                                        </p>                  
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
                 )
             }
             else {
                 return (
-                    <div></div>
+                    <div>Nothing to show
+                        {console.log(comments)}
+                    </div>
                 )
             }
-        })
-        return(
-            <>
-                {comt}
-            </>
-        )    
-    }
+        }
+          
+    
 
 
   const DishDetail = (props)=>{  
     if (props.dish !=null){   
     return(
                 <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to='/menu'>Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                 <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                 </div>
+             </div>
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
                     <RenderDish dish={props.dish}/>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <DishComments comments={props.dish}/>  
-                    </div>
+                    <RenderComments comments={props.comment}/>
+                    
                 </div>
                 </div>
         )
