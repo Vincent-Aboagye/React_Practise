d5838a8ee461582665727d6c03eccb63ff76acce
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-11-16 10:10:51 +0000

    Single Page Application

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -14,19 +14,16 @@ function RenderDish({dish}) {
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
                         </CardBody>
-
                     </Card>
                     {console.log(`${dish}`)}
                     </div>
-            )
-            }
+            )}
             else{
                 return (
-                <div>
+                    <div>
 
-                </div>
-                )
-                
+                    </div>
+                )    
             }    
             
     }
@@ -69,9 +66,14 @@ function RenderComments({comments}){
                     <div className="row">
                         <Breadcrumb>
                             <BreadcrumbItem>
+                                <Link to='/home'>Home</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem>
                                 <Link to='/menu'>Menu</Link>
                             </BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.dish.name}
+                            </BreadcrumbItem>
                         </Breadcrumb>
                  <div className="col-12">
                     <h3>{props.dish.name}</h3>
@@ -80,8 +82,7 @@ function RenderComments({comments}){
              </div>
                 <div className="row">
                     <RenderDish dish={props.dish}/>
-                    <RenderComments comments={props.comment}/>
-                    
+                    <RenderComments comments={props.comment}/>                
                 </div>
                 </div>
         )
